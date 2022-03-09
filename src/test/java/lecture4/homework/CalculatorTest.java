6b111a5fda9389ba9a22774f0dbf0d088e339aa9
Igors Mihejevs <mihejevs.igors@gmail.com>
2022-03-09 20:25:44 +0200

    CalculatorTest added

---
+++ src/test/java/lecture4/homework/CalculatorTest.java
@@ -0,0 +1,24 @@
+package lecture4.homework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void sum() {
+    }
+
+    @Test
+    void subtraction() {
+    }
+
+    @Test
+    void multiplication() {
+    }
+
+    @Test
+    void division() {
+    }
+}
