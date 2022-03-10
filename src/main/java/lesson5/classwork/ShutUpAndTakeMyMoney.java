a12edc6048a53c50fb907b8fdd986caf17272ddd
Igors Mihejevs <mihejevs.igors@gmail.com>
2022-03-10 21:03:00 +0200

    ShutUpAndTakeMyMoney added

---
+++ src/main/java/lesson5/classwork/ShutUpAndTakeMyMoney.java
@@ -0,0 +1,33 @@
+package lesson5.classwork;
+
+import java.util.Scanner;
+
+public class ShutUpAndTakeMyMoney {
+
+    public static void main(String[] args) {
+
+        int[] euros = {2, 1};
+        int[] cents = {50, 20, 10, 5, 2, 1};
+        int[] eurosCount = {0, 0};
+        int[] centsCount = {0, 0, 0, 0, 0, 0};
+
+        System.out.println("\nThe program calculate which coins and how many of them it will give you back");
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.print("Please enter amount -> ");
+        Scanner sc = new Scanner(System.in);
+        int amountOfMoney = sc.nextInt();
+        int amountOfEuro = amountOfMoney / 100;
+        int amountOfCents = amountOfMoney % 100;
+        int numberOfCents = 0;
+
+        eurosCount[0] = amountOfEuro / euros[0];
+        if (amountOfEuro % euros[0] == 1) eurosCount[1] = 1;
+
+        for (int i = 0; i < euros.length; i++) {
+            System.out.println("Number of " + euros[i] + " EUR = " + eurosCount[i]);
+        }
+
+
+    }
+
+}
