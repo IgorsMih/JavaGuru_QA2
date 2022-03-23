18105abc5d82475413c627097a46b4995e422b38
Igors Mihejevs <mihejevs.igors@gmail.com>
2022-03-23 19:03:09 +0200

    SSDogs refactoring

--- src/test/java/lesson7/SSDogs.java
+++ src/test/java/lesson7/SSDogs.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class OurFirstSelenium {
+public class SSDogs {
 
     @Test
     public void firstSelenium() {
