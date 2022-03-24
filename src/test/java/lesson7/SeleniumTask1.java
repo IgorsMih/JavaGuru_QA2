b8bb5618fb56c1d576bdaa5b36dc81f3edbdaa1e
Igors Mihejevs <mihejevs.igors@gmail.com>
2022-03-24 07:30:17 +0200

    Selenium files refactoring/added

---
+++ src/test/java/lesson7/SeleniumTask1.java
@@ -0,0 +1,45 @@
+package lesson7;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SeleniumTask1 {
+
+    @Test
+    public void firstSeleniumTest() {
+        String driverPath = "C:\\Java\\JavaGuru_QA2\\src\\test\\resources\\";
+        System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.ss.com/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("mtd_300")).click();
+        String currentUrl = driver.getCurrentUrl();
+        assertThat(currentUrl).isEqualTo("https://www.ss.com/lv/animals/dogs/");
+
+        Select dogType = new Select(driver.findElements(By.className("filter_sel")).get(1));
+        dogType.selectByValue("riga_f");
+// Find element age and enter some data
+        driver.findElement(By.id("f_o_1276_min")).sendKeys("1");
+// Find element meklet button by xpath
+        driver.findElement(By.xpath("//input[@class='b s12']")).click();
+// Get textbox text
+        String ageText = driver.findElement(By.id("f_o_1276_min")).getAttribute("value");
+        assertThat(ageText).isEqualTo("1");
+
+//        driver.findElement(By.cssSelector("input[class = 'b s12']")).click();
+        driver.findElement(By.xpath("//input[@class = 'b s12']")).click();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.quit();
+    }
+
+}
--- src/test/java/lesson7/SeleniumTask2.java
+++ src/test/java/lesson7/SeleniumTask2.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class SSAuto {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SeleniumTask2 {
 
     @Test
     public void secondSelenium() {
@@ -15,24 +17,32 @@ public class SSAuto {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.ss.com/lv/transport/cars/");
 
-        driver.findElement(By.id("f_o_8_min")).sendKeys("10000");
-        driver.findElement(By.id("f_o_8_max")).sendKeys("30000");
+        driver.findElement(By.id("f_o_8_min")).sendKeys("6000");
+        driver.findElement(By.id("f_o_8_max")).sendKeys("10000");
+        String priceMin = driver.findElement(By.id("f_o_8_min")).getAttribute("value");
+        assertThat(priceMin).isEqualTo("6000");
+        String priceMax = driver.findElement(By.id("f_o_8_max")).getAttribute("value");
+        assertThat(priceMax).isEqualTo("10000");
         Select minYear = new Select(driver.findElement(By.id("f_o_18_min")));
-        minYear.selectByValue("2020");
-        Select maxYear = new Select(driver.findElement(By.id("f_o_18_max")));
-        maxYear.selectByValue("2022");
+        minYear.selectByValue("2001");
+        String yearFrom = driver.findElement(By.id("f_o_18_min")).getAttribute("value");
+        assertThat(yearFrom).isEqualTo("2001");
         Select maxEngine = new Select(driver.findElement(By.id("f_o_15_max")));
         maxEngine.selectByValue("3.0");
+        String engineMax = driver.findElement(By.id("f_o_15_max")).getAttribute("value");
+        assertThat(engineMax).isEqualTo("3.0");
         Select colour = new Select(driver.findElement(By.id("f_o_17")));
         colour.selectByValue("6318");
-//        driver.findElement(By.cssSelector("input[class = 'b s12']")).click();
+        String colorBalta = driver.findElement(By.id("f_o_17")).getAttribute("value");
+        assertThat(colorBalta).isEqualTo("6318");
+
         driver.findElement(By.xpath("//input[@class = 'b s12']")).click();
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        driver.close();
+        driver.quit();
     }
 
 }
